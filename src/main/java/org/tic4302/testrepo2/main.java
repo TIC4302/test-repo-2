f109c3317fda3503e24f00ad8b8f475c64a12add
Aris Cahyadi Risdianto <aris.risdianto@gmail.com>
2021-02-18 19:46:20 +0800

    test commit to initiate build.

--- src/main/java/org/tic4302/testrepo2/main.java
+++ src/main/java/org/tic4302/testrepo2/main.java
@@ -3,6 +3,7 @@ package org.tic4302.testrepo2;
 public class main {
 
     String greetingString = "Hi Aris!";
+    // testing to initiate build
 
     public String getGreetingString(){
         return greetingString;
